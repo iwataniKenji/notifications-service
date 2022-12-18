45c12a6c38b3c418e6a4293f49e1e5ded137b0ca
iwatani-kenji <kleversonkenji@gmail.com>
2022-12-18 10:11:14 -0300

    adds getters and setters

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,47 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  // props facilita passagem de dados no construtor
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  // getters
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+  public get content(): string {
+    return this.props.content;
+  }
+  public get category(): string {
+    return this.props.category;
+  }
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+  public get createdAt(): Date | null | undefined {
+    return this.props.createdAt;
+  }
+
+  // setters
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+  public set content(content: string) {
+    this.props.content = content;
+  }
+  public set category(category: string) {
+    this.props.category = category;
+  }
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+}
